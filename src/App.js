ab284ceebd2541b6e708b3b95e78cf90b2b21aea
babinicz <babinicz@onet.pl>
2022-06-20 15:11:47 +0200

    ciasteczka

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Contact from "./components/contact/Contact";
 import Footer from "./components/common/footer/Footer";
 import ScrollToTop from "./ScrollToTop";
 import Scroll from "./components/Scroll";
+import CookieConsent from "react-cookie-consent";
 
 const App = () => {
   return (
@@ -30,6 +31,33 @@ const App = () => {
           <Route path="/kontakt" element={<Contact />} />
         </Routes>
         <Footer />
+        <CookieConsent
+          debug={true}
+          location="bottom"
+          style={{
+            background: "#333",
+            textAlign: "left",
+            paddingBottom: "1rem",
+            fontSize: "16px",
+            fontFamily: "Gideon Roman",
+          }}
+          buttonStyle={{
+            color: "#333",
+            background: "#fff",
+            fontSize: "18px",
+            fontFamily: "Gideon Roman",
+            marginRight: "1rem",
+          }}
+          buttonText="OK, rozumiem"
+          expires={365}
+        >
+          "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+          Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+          indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+          dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+          urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+          ustawień dotyczących cookies."
+        </CookieConsent>
       </Router>
     </>
   );
